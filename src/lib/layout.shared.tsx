dfa9e0a1e258ad45acf51cbd02ece9c977b207ab
PEEVEEz <zzpeeveezz@gmail.com>
2026-04-23 22:38:46 +0300

    first

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,5 +1,6 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { appName, gitConfig } from "./shared";
+import { PepiconsPopInternet, IcBaselineDiscord } from "@/components/icons";
 
 export function baseOptions(): BaseLayoutProps {
   return {
@@ -8,5 +9,19 @@ export function baseOptions(): BaseLayoutProps {
       title: appName,
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
+    links: [
+      {
+        icon: <PepiconsPopInternet />,
+        text: "Website",
+        type: "button",
+        url: "https://unityscripts.net",
+      },
+      {
+        icon: <IcBaselineDiscord />,
+        text: "Discord",
+        type: "button",
+        url: "https://discord.gg/unityscripts",
+      },
+    ],
   };
 }
